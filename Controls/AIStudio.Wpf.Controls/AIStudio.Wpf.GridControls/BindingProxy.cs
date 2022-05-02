82437544cebc6e3c3053a565ab4868131180738e
艾竹 <akwkevin@126.com>
2022-05-02 17:23:50 +0800

    FilterGrid样式

---
+++ Controls/AIStudio.Wpf.Controls/AIStudio.Wpf.GridControls/BindingProxy.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace AIStudio.Wpf.GridControls
+{
+    public class BindingProxy : Freezable
+    {
+        protected override System.Windows.Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        public object Data
+        {
+            get
+            {
+                return (object)GetValue(DataProperty);
+            }
+            set
+            {
+                SetValue(DataProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.
+        // This enables animation, styling, binding, etc...
+        public static readonly System.Windows.DependencyProperty DataProperty =
+            System.Windows.DependencyProperty.Register("Data", typeof(object),
+            typeof(BindingProxy), new System.Windows.UIPropertyMetadata(null));
+    }
+}
---
+++ Controls/AIStudio.Wpf.Controls/AIStudio.Wpf.GridControls/Converters/DynamicResourceWithConverterExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace AIStudio.Wpf.GridControls.Converters
+{
+    public class DynamicResourceWithConverterExtension : DynamicResourceExtension
+    {
+        public DynamicResourceWithConverterExtension()
+        {
+        }
+
+        public DynamicResourceWithConverterExtension(object resourceKey)
+                : base(resourceKey)
+        {
+        }
+
+        public IValueConverter Converter
+        {
+            get; set;
+        }
+        public object ConverterParameter
+        {
+            get; set;
+        }
+
+        public override object ProvideValue(IServiceProvider provider)
+        {
+            object value = base.ProvideValue(provider);
+            if (value != this && Converter != null)
+            {
+                Type targetType = null;
+                var target = (IProvideValueTarget)provider.GetService(typeof(IProvideValueTarget));
+                if (target != null)
+                {
+                    DependencyProperty targetDp = target.TargetProperty as DependencyProperty;
+                    if (targetDp != null)
+                    {
+                        targetType = targetDp.PropertyType;
+                    }
+                }
+                if (targetType != null)
+                    return Converter.Convert(value, targetType, ConverterParameter, CultureInfo.CurrentCulture);
+            }
+
+            return value;
+        }
+    }
+}
