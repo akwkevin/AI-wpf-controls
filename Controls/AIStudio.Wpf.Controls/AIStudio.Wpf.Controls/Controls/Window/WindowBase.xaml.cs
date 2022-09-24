30b7a73464f2df9bb62c646b6ca21ab3fb6872ef
艾竹 <akwkevin@126.com>
2022-09-24 08:48:25 +0800

    windowbase 最大化 参照hd解决

--- Controls/AIStudio.Wpf.Controls/AIStudio.Wpf.Controls/Controls/Window/WindowBase.xaml.cs
+++ Controls/AIStudio.Wpf.Controls/AIStudio.Wpf.Controls/Controls/Window/WindowBase.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Shapes;
 using AIStudio.Wpf.Controls.Helper;
 using WinInterop = System.Windows.Interop;
 using System.Runtime.InteropServices;
+using System.Windows.Shell;
+using AIStudio.Wpf.Controls.Interop;
 
 namespace AIStudio.Wpf.Controls
 {
@@ -49,6 +51,7 @@ namespace AIStudio.Wpf.Controls
         internal Grid overlayGrid;
         internal Grid waitingGrid;
         internal UIElement titleBar;
+        private readonly Thickness _commonPadding;
 
         static WindowBase()
         {
@@ -57,8 +60,15 @@ namespace AIStudio.Wpf.Controls
 
         public WindowBase()
         {
-            this.WindowStyle = WindowStyle.None;
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            var chrome = new WindowChrome
+            {
+                CornerRadius = new CornerRadius(),
+                GlassFrameThickness = new Thickness(0, 0, 0, 1),
+                UseAeroCaptionButtons = false
+            };
+
+            WindowChrome.SetWindowChrome(this, chrome);
+            _commonPadding = Padding;
 
             this.Icon = new BitmapImage(new Uri("pack://application:,,,/AIStudio.Wpf.Controls;component/Resources/A.ico", UriKind.RelativeOrAbsolute));
 
@@ -73,8 +83,6 @@ namespace AIStudio.Wpf.Controls
 
             Loaded += OnLoaded;
             Unloaded += OnUnloaded;
-
-            this.SourceInitialized += new EventHandler(win_SourceInitialized);
         }
 
         public override void OnApplyTemplate()
@@ -100,6 +108,7 @@ namespace AIStudio.Wpf.Controls
                 titleBar.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
             }
         }
+
 
         private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -1303,233 +1312,66 @@ namespace AIStudio.Wpf.Controls
 
         #region 最大化显示任务栏
 
-        void win_SourceInitialized(object sender, EventArgs e)
+        protected override void OnSourceInitialized(EventArgs e)
         {
-            System.IntPtr handle = (new WinInterop.WindowInteropHelper(this)).Handle;
-            WinInterop.HwndSource.FromHwnd(handle).AddHook(new WinInterop.HwndSourceHook(WindowProc));
+            base.OnSourceInitialized(e);
+            this.GetHwndSource()?.AddHook(HwndSourceHook);
         }
 
-        private System.IntPtr WindowProc(
-              System.IntPtr hwnd,
-              int msg,
-              System.IntPtr wParam,
-              System.IntPtr lParam,
-              ref bool handled)
+
+        private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
         {
             switch (msg)
             {
-                case 0x0024:
-                    WmGetMinMaxInfo(hwnd, lParam);
-                    handled = true;
+                case InteropValues.WM_WINDOWPOSCHANGED:
+                    Padding = WindowState == WindowState.Maximized ? WindowHelper.WindowMaximizedPadding : _commonPadding;
+                    break;
+                case InteropValues.WM_GETMINMAXINFO:
+                    WmGetMinMaxInfo(hwnd, lparam);
+                    Padding = WindowState == WindowState.Maximized ? WindowHelper.WindowMaximizedPadding : _commonPadding;
+                    break;
+                case InteropValues.WM_NCHITTEST:
+                    // for fixing #886
+                    // https://developercommunity.visualstudio.com/t/overflow-exception-in-windowchrome/167357
+                    try
+                    {
+                        _ = lparam.ToInt32();
+                    }
+                    catch (OverflowException)
+                    {
+                        handled = true;
+                    }
                     break;
             }
 
-            return (System.IntPtr)0;
+            return IntPtr.Zero;
         }
 
         private void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
         {
+            var mmi = (InteropValues.MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(InteropValues.MINMAXINFO));
+            var monitor = InteropMethods.MonitorFromWindow(hwnd, InteropValues.MONITOR_DEFAULTTONEAREST);
 
-            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-
-            // Adjust the maximized size and position to fit the work area of the correct monitor
-            int MONITOR_DEFAULTTONEAREST = 0x00000002;
-            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-
-            if (monitor != System.IntPtr.Zero)
+            if (monitor != IntPtr.Zero && mmi != null)
             {
-
-                MONITORINFO monitorInfo = new MONITORINFO();
-                GetMonitorInfo(monitor, monitorInfo);
-                RECT rcWorkArea = monitorInfo.rcWork;
-                RECT rcMonitorArea = monitorInfo.rcMonitor;
-
-                if (ToggleFullScreen)
+                InteropValues.APPBARDATA appBarData = default;
+                var autoHide = InteropMethods.SHAppBarMessage(4, ref appBarData) != 0;
+                if (autoHide)
                 {
-                    mmi.ptMaxPosition.x = rcMonitorArea.left;
-                    mmi.ptMaxPosition.y = rcMonitorArea.top;
-                    mmi.ptMaxSize.x = rcMonitorArea.right;
-                    mmi.ptMaxSize.y = rcMonitorArea.bottom;
-                }
-                else
-                {
-                    mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
-                    mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
-                    mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
-                    mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+                    var monitorInfo = default(InteropValues.MONITORINFO);
+                    monitorInfo.cbSize = (uint)Marshal.SizeOf(typeof(InteropValues.MONITORINFO));
+                    InteropMethods.GetMonitorInfo(monitor, ref monitorInfo);
+                    var rcWorkArea = monitorInfo.rcWork;
+                    var rcMonitorArea = monitorInfo.rcMonitor;
+                    mmi.ptMaxPosition.X = Math.Abs(rcWorkArea.Left - rcMonitorArea.Left);
+                    mmi.ptMaxPosition.Y = Math.Abs(rcWorkArea.Top - rcMonitorArea.Top);
+                    mmi.ptMaxSize.X = Math.Abs(rcWorkArea.Right - rcWorkArea.Left);
+                    mmi.ptMaxSize.Y = Math.Abs(rcWorkArea.Bottom - rcWorkArea.Top - 1);
                 }
             }
 
             Marshal.StructureToPtr(mmi, lParam, true);
-        }
-
-
-        /// <summary>
-        /// POINT aka POINTAPI
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            /// <summary>
-            /// x coordinate of point.
-            /// </summary>
-            public int x;
-            /// <summary>
-            /// y coordinate of point.
-            /// </summary>
-            public int y;
-
-            /// <summary>
-            /// Construct a point of coordinates (x,y).
-            /// </summary>
-            public POINT(int x, int y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO
-        {
-            public POINT ptReserved;
-            public POINT ptMaxSize;
-            public POINT ptMaxPosition;
-            public POINT ptMinTrackSize;
-            public POINT ptMaxTrackSize;
-        };
-
-
-
-        /// <summary>
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        public class MONITORINFO
-        {
-            /// <summary>
-            /// </summary>            
-            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcMonitor = new RECT();
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcWork = new RECT();
-
-            /// <summary>
-            /// </summary>            
-            public int dwFlags = 0;
-        }
-
-
-        /// <summary> Win32 </summary>
-        [StructLayout(LayoutKind.Sequential, Pack = 0)]
-        public struct RECT
-        {
-            /// <summary> Win32 </summary>
-            public int left;
-            /// <summary> Win32 </summary>
-            public int top;
-            /// <summary> Win32 </summary>
-            public int right;
-            /// <summary> Win32 </summary>
-            public int bottom;
-
-            /// <summary> Win32 </summary>
-            public static readonly RECT Empty = new RECT();
-
-            /// <summary> Win32 </summary>
-            public int Width
-            {
-                get
-                {
-                    return Math.Abs(right - left);
-                }  // Abs needed for BIDI OS
-            }
-            /// <summary> Win32 </summary>
-            public int Height
-            {
-                get
-                {
-                    return bottom - top;
-                }
-            }
-
-            /// <summary> Win32 </summary>
-            public RECT(int left, int top, int right, int bottom)
-            {
-                this.left = left;
-                this.top = top;
-                this.right = right;
-                this.bottom = bottom;
-            }
-
-
-            /// <summary> Win32 </summary>
-            public RECT(RECT rcSrc)
-            {
-                this.left = rcSrc.left;
-                this.top = rcSrc.top;
-                this.right = rcSrc.right;
-                this.bottom = rcSrc.bottom;
-            }
-
-            /// <summary> Win32 </summary>
-            public bool IsEmpty
-            {
-                get
-                {
-                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
-                    return left >= right || top >= bottom;
-                }
-            }
-            /// <summary> Return a user friendly representation of this struct </summary>
-            public override string ToString()
-            {
-                if (this == RECT.Empty) { return "RECT {Empty}"; }
-                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
-            }
-
-            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
-            public override bool Equals(object obj)
-            {
-                if (!(obj is Rect)) { return false; }
-                return (this == (RECT)obj);
-            }
-
-            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
-            public override int GetHashCode()
-            {
-                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
-            }
-
-
-            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
-            public static bool operator ==(RECT rect1, RECT rect2)
-            {
-                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
-            }
-
-            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
-            public static bool operator !=(RECT rect1, RECT rect2)
-            {
-                return !(rect1 == rect2);
-            }
-
-
-        }
-
-        [DllImport("user32")]
-        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("User32")]
-        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
-
+        }     
         #endregion
 
         public static readonly DependencyProperty ShowNotifyIconProperty = DependencyProperty.Register(
