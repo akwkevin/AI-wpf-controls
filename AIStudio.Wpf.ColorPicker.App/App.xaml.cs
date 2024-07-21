936e18052a41fa27280f7c7951cf955d4727188c
艾竹 <akwkevin@126.com>
2024-07-21 21:50:49 +0800

    取色器

---
+++ AIStudio.Wpf.ColorPicker.App/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using AIStudio.Wpf.ColorPicker.ViewModels;
+
+namespace AIStudio.Wpf.ColorPicker.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+        }
+        protected void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                AIStudio.Wpf.ColorPicker.MainWindow window = new AIStudio.Wpf.ColorPicker.MainWindow();
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+            
+            }
+
+        }
+    }
+
+
+
+}
